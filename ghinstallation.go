aa1489096b0e46705011c530a2a065ea62e342a0
Jesse Hathaway <jesse@mbuki-mvuki.org>
2019-10-30 11:23:23 -0500

    ghe app test

---
+++ ghinstallation.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/google/go-github/github"
+)
+
+const GitHubEnterpriseURL = "https://github.braintreeps.com/api/v3"
+
+func main() {
+	// Shared transport to reuse TCP connections.
+	tr := http.DefaultTransport
+
+	// Wrap the shared transport for use with the app ID 1 authenticating with installation ID 99.
+	itr, err := ghinstallation.NewKeyFromFile(tr, 7, 99, "heckler.2019-10-30.private-key.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+	itr.BaseURL = GitHubEnterpriseURL
+
+	// Use installation transport with github.com/google/go-github
+	client, err := github.NewEnterpriseClient(GitHubEnterpriseURL, GitHubEnterpriseURL, &http.Client{Transport: itr})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	orgs, _, err := client.Organizations.List(context.Background(), "lollipopman", nil)
+	fmt.Printf("orgs %v\n", orgs)
+
+	// list public repositories for org "github"
+	opt := &github.RepositoryListByOrgOptions{Type: "public"}
+	repos, _, err := client.Repositories.ListByOrg(context.Background(), "braintree", opt)
+
+	fmt.Printf("repos %v\n", repos)
+}
