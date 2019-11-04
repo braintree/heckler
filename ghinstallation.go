3080247b37570c283478298dcf0c6ffa8e497510
Jesse Hathaway <jesse@mbuki-mvuki.org>
2019-11-04 10:54:03 -0600

    more!

--- ghinstallation.go
+++ ghinstallation.go
@@ -28,19 +28,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	orgs, _, err := client.Organizations.List(context.Background(), "lollipopman", nil)
+	ctx := context.Background()
+	//r := &github.Repository{Name: github.String("potto"), Private: github.Bool(false), Description: github.String("description")}
+	//func (s *IssuesService) Create(ctx context.Context, owner string, repo string, issue *IssueRequest) (*Issue, *Response, error)
+	//func (s *IssuesService) CreateMilestone(ctx context.Context, owner string, repo string, milestone *Milestone) (*Milestone, *Response, error)
+	//
+	m := &github.Milestone{
+		Title: github.String("v1.1"),
+	}
+	nm, _, err := client.Issues.CreateMilestone(ctx, "lollipopman", "muppetshow", m)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("orgs %v\n", orgs)
+	fmt.Printf("Successfully created new milestone: %v\n", *nm.Title)
 
-	// list public repositories for org "github"
-	opt := &github.RepositoryListByOrgOptions{Type: "public"}
-	repos, _, err := client.Repositories.ListByOrg(context.Background(), "braintree", opt)
+	i := &github.IssueRequest{
+		Title:     github.String("gonzo3"),
+		Assignee:  github.String("lollipopman"),
+		Body:      github.String("```bash\n$ ps -ef\n```\n"),
+		Milestone: nm.Number,
+	}
+	ni, _, err := client.Issues.Create(ctx, "lollipopman", "muppetshow", i)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Printf("repos %v\n", repos)
+	fmt.Printf("Successfully created new issue: %v\n", *ni.Title)
 }
