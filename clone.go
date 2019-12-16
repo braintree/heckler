45d735dc6ddc8eeb2d9753cdc5438150c2da66c5
Jesse Hathaway <jesse@mbuki-mvuki.org>
2019-12-16 16:04:33 +0000

    add gitutil library

--- clone.go
+++ clone.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"path"
+
+	"./gitutil"
 
 	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/google/go-github/github"
@@ -15,136 +14,6 @@ import (
 )
 
 const GitHubEnterpriseURL = "https://github.braintreeps.com/api/v3"
-
-func clone(remoteUrl string, cloneDir string, cloneOptions *git.CloneOptions) (*git.Repository, error) {
-	var repo *git.Repository
-	var err error
-
-	if _, err = os.Stat(path.Join(cloneDir, ".git")); os.IsNotExist(err) {
-		fmt.Println("cloning")
-		repo, err = git.Clone(remoteUrl, cloneDir, cloneOptions)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		repo, err = git.OpenRepository(cloneDir)
-		if err != nil {
-			return nil, err
-		}
-	}
-	err = repo.Remotes.SetUrl("origin", remoteUrl)
-	if err != nil {
-		return nil, err
-	}
-	return repo, nil
-}
-
-func fastForward(repo *git.Repository) error {
-	remote, err := repo.Remotes.Lookup("origin")
-	if err != nil {
-		return err
-	}
-
-	// XXX only fetch specific branch?
-	err = remote.Fetch([]string{}, nil, "")
-	if err != nil {
-		return err
-	}
-
-	// open master branch of remote
-	remoteBranch, err := repo.References.Lookup("refs/remotes/origin/master")
-	if err != nil {
-		return err
-	}
-
-	// grab commit from head of branch, use annotated variety which
-	// includes what ref it was looked up from
-	annotatedCommit, err := repo.AnnotatedCommitFromRef(remoteBranch)
-	if err != nil {
-		return err
-	}
-
-	// Do the merge analysis
-	mergeHeads := make([]*git.AnnotatedCommit, 1)
-	mergeHeads[0] = annotatedCommit
-	analysis, _, err := repo.MergeAnalysis(mergeHeads)
-	if err != nil {
-		return err
-	}
-	remoteBranchID := remoteBranch.Target()
-	remoteBranchObj, err := repo.Lookup(remoteBranchID)
-	if err != nil {
-		return err
-	}
-
-	remoteBranchCommit, err := remoteBranchObj.AsCommit()
-	if err != nil {
-		return err
-	}
-
-	if (analysis & git.MergeAnalysisUpToDate) != 0 {
-		fmt.Println("Already up to date")
-		return nil
-	}
-
-	if (analysis & git.MergeAnalysisFastForward) == 0 {
-		fmt.Println("Not a fast forward, bailing")
-		return errors.New("Not a fast forward, bailing")
-	}
-
-	fmt.Printf("Fast forward...")
-	// Fast-forward changes
-	// Get remote tree
-	remoteTree, err := repo.LookupTree(remoteBranchCommit.TreeId())
-	if err != nil {
-		return err
-	}
-
-	// Checkout
-	checkoutOpts := git.CheckoutOpts{
-		Strategy: git.CheckoutSafe,
-	}
-	err = repo.CheckoutTree(remoteTree, &checkoutOpts)
-	if err != nil {
-		return err
-	}
-
-	branchRef, err := repo.References.Lookup("refs/heads/master")
-	if err != nil {
-		return err
-	}
-
-	// Point branch to the same object as the remote branch
-	_, err = branchRef.SetTarget(remoteBranchID, "")
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Success")
-	return nil
-}
-
-func walk(repo *git.Repository) error {
-	var err error
-
-	rv, err := repo.Walk()
-	rv.Sorting(git.SortTopological)
-	if err != nil {
-		return err
-	}
-	err = rv.PushRef("refs/tags/v2")
-	err = rv.HideRef("refs/tags/v1")
-	if err != nil {
-		return err
-	}
-
-	var gi git.Oid
-	for rv.Next(&gi) == nil {
-		fmt.Printf("%v\n", gi.String())
-	}
-
-	return nil
-}
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -174,15 +43,15 @@ func main() {
 	cloneDir := "/home/admin/tmp/muppetshow"
 	cloneOptions := &git.CloneOptions{}
 	remoteUrl := fmt.Sprintf("https://x-access-token:%s@github.braintreeps.com/lollipopman/muppetshow", tok)
-	repo, err := clone(remoteUrl, cloneDir, cloneOptions)
+	repo, err := gitutil.Clone(remoteUrl, cloneDir, cloneOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = fastForward(repo)
+	err = gitutil.FastForward(repo)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = walk(repo)
+	err = gitutil.Walk(repo)
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ gitutil/gitutil.go
@@ -0,0 +1,140 @@
+package gitutil
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+
+	git "github.com/libgit2/git2go"
+)
+
+func Clone(remoteUrl string, cloneDir string, cloneOptions *git.CloneOptions) (*git.Repository, error) {
+	var repo *git.Repository
+	var err error
+
+	if _, err = os.Stat(path.Join(cloneDir, ".git")); os.IsNotExist(err) {
+		fmt.Println("cloning")
+		repo, err = git.Clone(remoteUrl, cloneDir, cloneOptions)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		repo, err = git.OpenRepository(cloneDir)
+		if err != nil {
+			return nil, err
+		}
+	}
+	err = repo.Remotes.SetUrl("origin", remoteUrl)
+	if err != nil {
+		return nil, err
+	}
+	return repo, nil
+}
+
+func FastForward(repo *git.Repository) error {
+	remote, err := repo.Remotes.Lookup("origin")
+	if err != nil {
+		return err
+	}
+
+	// XXX only fetch specific branch?
+	err = remote.Fetch([]string{}, nil, "")
+	if err != nil {
+		return err
+	}
+
+	// open master branch of remote
+	remoteBranch, err := repo.References.Lookup("refs/remotes/origin/master")
+	if err != nil {
+		return err
+	}
+
+	// grab commit from head of branch, use annotated variety which
+	// includes what ref it was looked up from
+	annotatedCommit, err := repo.AnnotatedCommitFromRef(remoteBranch)
+	if err != nil {
+		return err
+	}
+
+	// Do the merge analysis
+	mergeHeads := make([]*git.AnnotatedCommit, 1)
+	mergeHeads[0] = annotatedCommit
+	analysis, _, err := repo.MergeAnalysis(mergeHeads)
+	if err != nil {
+		return err
+	}
+	remoteBranchID := remoteBranch.Target()
+	remoteBranchObj, err := repo.Lookup(remoteBranchID)
+	if err != nil {
+		return err
+	}
+
+	remoteBranchCommit, err := remoteBranchObj.AsCommit()
+	if err != nil {
+		return err
+	}
+
+	if (analysis & git.MergeAnalysisUpToDate) != 0 {
+		fmt.Println("Already up to date")
+		return nil
+	}
+
+	if (analysis & git.MergeAnalysisFastForward) == 0 {
+		fmt.Println("Not a fast forward, bailing")
+		return errors.New("Not a fast forward, bailing")
+	}
+
+	fmt.Printf("Fast forward...")
+	// Fast-forward changes
+	// Get remote tree
+	remoteTree, err := repo.LookupTree(remoteBranchCommit.TreeId())
+	if err != nil {
+		return err
+	}
+
+	// Checkout
+	checkoutOpts := git.CheckoutOpts{
+		Strategy: git.CheckoutSafe,
+	}
+	err = repo.CheckoutTree(remoteTree, &checkoutOpts)
+	if err != nil {
+		return err
+	}
+
+	branchRef, err := repo.References.Lookup("refs/heads/master")
+	if err != nil {
+		return err
+	}
+
+	// Point branch to the same object as the remote branch
+	_, err = branchRef.SetTarget(remoteBranchID, "")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Success")
+	return nil
+}
+
+func Walk(repo *git.Repository) error {
+	var err error
+
+	rv, err := repo.Walk()
+	rv.Sorting(git.SortTopological)
+	if err != nil {
+		return err
+	}
+	err = rv.PushRef("refs/tags/v2")
+	err = rv.HideRef("refs/tags/v1")
+	if err != nil {
+		return err
+	}
+
+	var gi git.Oid
+	for rv.Next(&gi) == nil {
+		fmt.Printf("%v\n", gi.String())
+	}
+
+	return nil
+}
