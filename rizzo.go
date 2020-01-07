ca270083284a1b2b2ea4910792eabf46843e7e2b
Jesse Hathaway <jesse@mbuki-mvuki.org>
2020-01-07 21:38:22 +0000

    initial rizzo client, with a topological walk

--- rizzo.go
+++ rizzo.go
@@ -1,67 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"os/exec"
 
+	"./gitutil"
 	git "github.com/libgit2/git2go"
 )
-
-func main() {
-	// log.SetFlags(log.LstdFlags | log.Lshortfile)
-	//
-	// // Shared transport to reuse TCP connections.
-	// tr := http.DefaultTransport
-	//
-	// // Wrap the shared transport for use with the app ID 7 authenticating with
-	// // installation ID 11.
-	// itr, err := ghinstallation.NewKeyFromFile(tr, 7, 11, "heckler.2019-10-30.private-key.pem")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// itr.BaseURL = GitHubEnterpriseURL
-	//
-	// // Use installation transport with github.com/google/go-github
-	// _, err = github.NewEnterpriseClient(GitHubEnterpriseURL, GitHubEnterpriseURL, &http.Client{Transport: itr})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// ctx := context.Background()
-	// tok, err := itr.Token(ctx)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// cloneDir := "/home/admin/tmp/muppetshow"
-	// cloneOptions := &git.CloneOptions{}
-	// remoteUrl := fmt.Sprintf("https://x-access-token:%s@github.braintreeps.com/lollipopman/muppetshow", tok)
-	// repo, err := gitutil.Clone(remoteUrl, cloneDir, cloneOptions)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// err = gitutil.FastForward(repo)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// err = gitutil.Walk(repo)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	cloneOptions := &git.CloneOptions{}
-	// use FetchOptions instead of directly RemoteCallbacks
-	// https://github.com/libgit2/git2go/commit/36e0a256fe79f87447bb730fda53e5cbc90eb47c
-	cloneOptions.FetchOptions = &git.FetchOptions{
-		RemoteCallbacks: git.RemoteCallbacks{
-			CredentialsCallback:      makeCredentialsCallback(),
-			CertificateCheckCallback: certificateCheckCallback,
-		},
-	}
-
-	_, err := git.Clone("root@heckler:/data/muppetshow", "/data/muppetshow", cloneOptions)
-	if err != nil {
-		log.Panic(err)
-	}
-	log.Print("SUCESS")
-}
 
 func makeCredentialsCallback() git.CredentialsCallback {
 	called := false
@@ -75,7 +22,7 @@ func makeCredentialsCallback() git.CredentialsCallback {
 		called = true
 		// homeDir := os.UserHomeDir() need golang 1.12
 		homeDir := "/root"
-		// if allowedTypes&git.CredTypeSshKey != 0 {
+		// XXX do we need to check git.CredType? allowedTypes&git.CredTypeSshKey != 0
 		errCode, cred := git.NewCredSshKey(username, homeDir+"/.ssh/id_ecdsa.pub", homeDir+"/.ssh/id_ecdsa", "")
 		return git.ErrorCode(errCode), &cred
 	}
@@ -83,5 +30,109 @@ func makeCredentialsCallback() git.CredentialsCallback {
 
 // Made this one just return 0 during troubleshooting...
 func certificateCheckCallback(cert *git.Certificate, valid bool, hostname string) git.ErrorCode {
+	// XXX implement host key checking
 	return git.ErrOk
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	var err error
+
+	cloneOptions := &git.CloneOptions{}
+	cloneOptions.FetchOptions = &git.FetchOptions{
+		RemoteCallbacks: git.RemoteCallbacks{
+			CredentialsCallback:      makeCredentialsCallback(),
+			CertificateCheckCallback: certificateCheckCallback,
+		},
+	}
+
+	repo, err := gitutil.CloneOrOpen("root@heckler:/data/muppetshow", "/data/muppetshow", cloneOptions)
+	if err != nil {
+		log.Panic(err)
+	}
+	err = gitutil.FastForward(repo, cloneOptions.FetchOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rv, err := repo.Walk()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rv.Sorting(git.SortTopological)
+	err = rv.PushHead()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = rv.HideRef("refs/tags/v1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var gi git.Oid
+	for rv.Next(&gi) == nil {
+		fmt.Printf("Commit: %v\n", gi.String())
+		commit, err := repo.LookupCommit(&gi)
+		if err != nil {
+			log.Fatal(err)
+		}
+		tree, err := repo.LookupTree(commit.TreeId())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		checkoutOpts := git.CheckoutOpts{
+			Strategy: git.CheckoutSafe,
+		}
+		err = repo.CheckoutTree(tree, &checkoutOpts)
+		if err != nil {
+			log.Fatal(err)
+		}
+		index, err := repo.Index()
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = index.ReadTree(tree)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = index.Write()
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = repo.SetHeadDetached(&gi)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// puppet
+		repoDir := "/data/muppetshow"
+		puppetArgs := []string{
+			"apply",
+			"--noop",
+			"--confdir",
+			repoDir,
+			"--vardir",
+			"/var/tmp",
+			"--config_version",
+			"/heckler/git-head-sha",
+			repoDir + "/nodes.pp",
+		}
+		cmd := exec.Command("puppet", puppetArgs...)
+		hostname, err := os.Hostname()
+		if err != nil {
+			log.Fatal(err)
+		}
+		cmd.Env = append(os.Environ(),
+			"FACTER_nodename="+hostname,
+			"FACTER_cwd=/root",
+			"PUPPET_COMMIT="+gi.String(),
+		)
+		stdoutStderr, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("%s\n", stdoutStderr)
+			log.Fatal(err)
+		}
+	}
+	fmt.Println("Success")
 }
