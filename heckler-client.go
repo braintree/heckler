8980d0b1d02217558ba1f665ed55b583d4bafc58
Jesse Hathaway <jesse@mbuki-mvuki.org>
2020-01-15 16:25:11 +0000

    add clone support

--- heckler-client.go
+++ heckler-client.go
@@ -6,11 +6,18 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"time"
 
+	"./gitutil"
 	"./puppetutil"
+	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/google/go-github/github"
+	git "github.com/libgit2/git2go"
 	"google.golang.org/grpc"
 )
+
+const GitHubEnterpriseURL = "https://github.braintreeps.com/api/v3"
 
 type hostFlags []string
 
@@ -21,6 +28,44 @@ func (i *hostFlags) String() string {
 func (i *hostFlags) Set(value string) error {
 	*i = append(*i, value)
 	return nil
+}
+
+func fetchRepo() (*git.Repository, error) {
+
+	// Shared transport to reuse TCP connections.
+	tr := http.DefaultTransport
+
+	// Wrap the shared transport for use with the app ID 7 authenticating with
+	// installation ID 11.
+	itr, err := ghinstallation.NewKeyFromFile(tr, 7, 11, "heckler.2019-10-30.private-key.pem")
+	if err != nil {
+		return nil, err
+	}
+	itr.BaseURL = GitHubEnterpriseURL
+
+	// Use installation transport with github.com/google/go-github
+	_, err = github.NewEnterpriseClient(GitHubEnterpriseURL, GitHubEnterpriseURL, &http.Client{Transport: itr})
+	if err != nil {
+		return nil, err
+	}
+	ctx := context.Background()
+	tok, err := itr.Token(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	cloneDir := "/data/muppetshow"
+	cloneOptions := &git.CloneOptions{}
+	remoteUrl := fmt.Sprintf("https://x-access-token:%s@github.braintreeps.com/lollipopman/muppetshow", tok)
+	repo, err := gitutil.CloneOrOpen(remoteUrl, cloneDir, cloneOptions)
+	if err != nil {
+		return nil, err
+	}
+	err = gitutil.FastForward(repo, nil)
+	if err != nil {
+		return nil, err
+	}
+	return repo, nil
 }
 
 func hecklerApply(rc puppetutil.RizzoClient, c chan<- puppetutil.PuppetReport, par puppetutil.PuppetApplyRequest) {
@@ -45,6 +90,15 @@ func main() {
 	flag.StringVar(&beginRev, "begin", "", "begin rev")
 	flag.BoolVar(&noop, "noop", false, "noop")
 	flag.Parse()
+
+	if len(hosts) == 0 {
+		log.Fatalf("must supply node list")
+	}
+
+	_, err := fetchRepo()
+	if err != nil {
+		log.Fatalf("Unable to fetch repo: %v", err)
+	}
 
 	// Set up a connection to the server.
 	for _, host := range hosts {
